945d484b54f04ddf9e9930ae714cb6c4f4ca2ee7
Hristo Dobtchev <hristo.dobtchev@sap.com>
2018-10-30 16:54:36 +0200

    Tabs fix

--- src/com/sap/calcacademy/RecursiveCalc.java
+++ src/com/sap/calcacademy/RecursiveCalc.java
@@ -1,22 +1,22 @@
 package com.sap.calcacademy;
 
 public class RecursiveCalc {
-	
+
 	public double calculate(String expression) throws Exception {
 		double result = 0;
 		Number oper = null;
 		int sign = 1;
 		char modOperation = 0;
-		
+
 		char[] expr = expression.toCharArray();
-		for(int i=0; i < expr.length && expr[i] != ')'; ++i) {
+		for (int i = 0; i < expr.length && expr[i] != ')'; ++i) {
 			char c = expr[i];
 			if (Character.isDigit(c)) {
 				double val = Character.digit(c, 10);
 				oper = calculateOperand(oper, modOperation, val);
 				modOperation = 0;
 			} else if (c == '(') {
-				oper = calculateOperand(oper, modOperation, calculate(expression.substring(i+1)));
+				oper = calculateOperand(oper, modOperation, calculate(expression.substring(i + 1)));
 				int idx = expression.indexOf(')', i);
 				if (idx == -1)
 					throw new Exception("Wrong parentesis count");
@@ -30,31 +30,31 @@ public class RecursiveCalc {
 			} else {
 				modOperation = c;
 			}
-			
+
 		}
 		result += sign * oper.doubleValue();
 		return result;
 	}
 
 	private double calculateOperand(Number oper, char modOperation, double val) throws Exception {
-		return (oper == null || modOperation == 0) ? val:execute(oper.doubleValue(), val, modOperation);
+		return (oper == null || modOperation == 0) ? val : execute(oper.doubleValue(), val, modOperation);
 	}
-	
+
 	private boolean isAddOrSubstract(char oper) {
 		return oper == '+' || oper == '-';
 	}
-	
+
 	private double execute(double oper1, double oper2, char operation) throws Exception {
-		switch(operation) {
-			case '*':
-				return oper1 * oper2;
-			case '/':
-				return oper1 / oper2;
-			default:
-				throw new Exception("Wrong operation");
+		switch (operation) {
+		case '*':
+			return oper1 * oper2;
+		case '/':
+			return oper1 / oper2;
+		default:
+			throw new Exception("Wrong operation");
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		RecursiveCalc calc = new RecursiveCalc();
 		try {
